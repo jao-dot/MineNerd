4c5ad8c50d0a5cea6a6ddf71ec05b830d8fe22ff
jao-dot <vitorjoao9346@gmail.com>
2024-11-26 20:03:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biomas',
+    'Quanros biomas existem no minecraft?',
+    'Existem 17 biomas no total do jogo inteiro'
+)
+
+criaCartao(
+    'Mineração',
+    'Qual é a melhor camada para encontrar Lápis-Lazuli?',
+    'A camada ideal para encontrar Lápis-Lazuli é a camada 0'
+)
+
+criaCartao(
+    'Warden',
+    'Quanto de vida tem um Warden?',
+    '500 corações de vida'
+)
+
+criaCartao(
+    'Drop',
+    'Qual a chance de um esqueleto-whiter dropar uma cabeça de esqueleto-whiter?',
+    '5% de chance'
+)
